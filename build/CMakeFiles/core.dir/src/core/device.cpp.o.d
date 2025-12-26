2a2c233e46eaead323788f1245dfc2ca151b4933
Labrapuerta <jmanuelhlabra@hotmail.com>
2025-12-26 21:20:13 +0100

    Remove local log files from commit message

--- build/CMakeFiles/core.dir/src/core/device.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/core.dir/src/core/device.cpp.o: \
-  /Users/manueldelabra/Documents/Portfolio/MolGrad/src/core/device.cpp \
-  /Users/manueldelabra/Documents/Portfolio/MolGrad/src/core/device.h
